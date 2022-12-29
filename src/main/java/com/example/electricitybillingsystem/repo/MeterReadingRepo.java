21fc91642c9b03dd5a2d52fe2b028fa3e8619208
Kevin Muchene <muchenekevin@gmail.com>
2022-12-28 20:05:02 -0600

    extends CrudRepository. Created findAll and findById

--- src/main/java/com/example/electricitybillingsystem/repo/MeterReadingRepo.java
+++ src/main/java/com/example/electricitybillingsystem/repo/MeterReadingRepo.java
@@ -1,4 +1,13 @@
 package com.example.electricitybillingsystem.repo;
 
-public interface MeterReadingRepo {
+import com.example.electricitybillingsystem.entity.MeterReading;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MeterReadingRepo extends CrudRepository<MeterReading, Long> {
+
+    public List<MeterReading> findByAll();
+
+    public MeterReading findById(Integer id);
 }
