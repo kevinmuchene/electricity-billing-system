b7d4afe2ba58c173d3a7ebfde0d5763172de359c
Kevin Muchene <muchenekevin@gmail.com>
2022-12-28 19:40:57 -0600

    extends CrudRepository. Created findAll and findByAddressId

--- src/main/java/com/example/electricitybillingsystem/repo/AddressRepo.java
+++ src/main/java/com/example/electricitybillingsystem/repo/AddressRepo.java
@@ -1,4 +1,13 @@
 package com.example.electricitybillingsystem.repo;
 
-public interface AddressRepo {
+import com.example.electricitybillingsystem.entity.Address;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AddressRepo extends CrudRepository<Address, Long> {
+
+    public List<Address> findAll();
+
+    public Address findByAddressId(Long id);
 }
