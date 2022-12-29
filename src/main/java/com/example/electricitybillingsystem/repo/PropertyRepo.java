5831e574a2ce35607c1f4ac3c7d47820fb15d9e9
Kevin Muchene <muchenekevin@gmail.com>
2022-12-28 20:15:19 -0600

    Extends CrudRepository, created findByAll, findById, findByCustomerDrivingLicence, findByCustomerId, findByState

--- src/main/java/com/example/electricitybillingsystem/repo/PropertyRepo.java
+++ src/main/java/com/example/electricitybillingsystem/repo/PropertyRepo.java
@@ -1,4 +1,21 @@
 package com.example.electricitybillingsystem.repo;
 
-public interface PropertyRepo {
+import com.example.electricitybillingsystem.entity.Property;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PropertyRepo extends CrudRepository<Property, Long> {
+
+    public List<Property> findByAll();
+
+    public Property findById(Integer id);
+
+    public List<Property> findByCustomerDrivingLicence(Integer drivingLicence);
+
+    public List<Property> findByCustomerId(Integer id);
+
+    public List<Property> findByState(String state);
+
+
 }
