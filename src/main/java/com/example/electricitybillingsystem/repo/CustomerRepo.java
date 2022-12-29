c37eb7085c5f574c0a8d531c09aa918754626a2b
Kevin Muchene <muchenekevin@gmail.com>
2022-12-28 19:48:47 -0600

    extends CrudRepository. Created findAll and findByCustomerId, findByProperty and findByBill

--- src/main/java/com/example/electricitybillingsystem/repo/CustomerRepo.java
+++ src/main/java/com/example/electricitybillingsystem/repo/CustomerRepo.java
@@ -1,4 +1,17 @@
 package com.example.electricitybillingsystem.repo;
 
-public interface CustomerRepo {
+import com.example.electricitybillingsystem.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepo extends CrudRepository<Customer, Long> {
+
+    public List<Customer> findByAll();
+
+    public List<Customer> findById(Integer id);
+
+    public Customer findByProperty(Integer id);
+
+    public Customer findByBill(Integer id);
 }
