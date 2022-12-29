b2d17c6ba491b19e344485571487ec102b6f0505
Kevin Muchene <muchenekevin@gmail.com>
2022-12-28 19:44:44 -0600

    extends CrudRepository. Created findAll and findByCustomerId, findByMonth

--- src/main/java/com/example/electricitybillingsystem/repo/BillRepo.java
+++ src/main/java/com/example/electricitybillingsystem/repo/BillRepo.java
@@ -1,4 +1,16 @@
 package com.example.electricitybillingsystem.repo;
 
-public interface BillRepo {
+import com.example.electricitybillingsystem.entity.Bill;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.Month;
+import java.util.List;
+
+public interface BillRepo extends CrudRepository<Bill, Long> {
+
+    public List<Bill> findAll();
+
+    public List<Bill> findByCustomerId(Long id);
+
+    public List<Bill> findByMonth(Month month);
 }
