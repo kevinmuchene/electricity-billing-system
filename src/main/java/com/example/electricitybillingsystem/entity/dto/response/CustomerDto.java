a69d34174529192a9588d9b6aea3b9c3a2feb392
Kevin Muchene <muchenekevin@gmail.com>
2022-12-30 21:14:41 -0600

    Initial commit for the dtos

--- src/main/java/com/example/electricitybillingsystem/entity/dto/response/CustomerDto.java
+++ src/main/java/com/example/electricitybillingsystem/entity/dto/response/CustomerDto.java
@@ -1,4 +1,16 @@
 package com.example.electricitybillingsystem.entity.dto.response;
 
+import com.example.electricitybillingsystem.entity.Address;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CustomerDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer licence;
+    private Address address;
 }
