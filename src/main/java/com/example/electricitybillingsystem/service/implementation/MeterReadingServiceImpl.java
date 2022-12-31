e39f879e5f8c212d0b47c9583ac8e3815db0012e
Kevin Muchene <muchenekevin@gmail.com>
2022-12-31 11:14:51 -0600

    implemented MeterReadingService methods

--- src/main/java/com/example/electricitybillingsystem/service/implementation/MeterReadingServiceImpl.java
+++ src/main/java/com/example/electricitybillingsystem/service/implementation/MeterReadingServiceImpl.java
@@ -1,4 +1,74 @@
 package com.example.electricitybillingsystem.service.implementation;
 
-public class MeterReadingServiceImpl {
+import com.example.electricitybillingsystem.entity.MeterReading;
+import com.example.electricitybillingsystem.entity.dto.response.MeterReadingDto;
+import com.example.electricitybillingsystem.repo.MeterReadingRepo;
+import com.example.electricitybillingsystem.service.MeterReadingService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MeterReadingServiceImpl implements MeterReadingService {
+
+    @Autowired
+    private MeterReadingRepo meterReadingRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public List<MeterReadingDto> findAll() {
+
+        var meterReadings = meterReadingRepo.findAll();
+
+        return meterReadings
+                .stream()
+                .map(meterReading -> modelMapper.map(meterReading, MeterReadingDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<MeterReadingDto> findMeterReadingByPropertyId(Long id) {
+
+        var meterReadings = meterReadingRepo.findMeterReadingByPropertyId(id);
+
+        return meterReadings
+                .stream()
+                .map(meterReading -> modelMapper.map(meterReading, MeterReadingDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(MeterReadingDto meterReadingDto) {
+        meterReadingRepo.save(modelMapper.map(meterReadingDto, MeterReading.class));
+    }
+
+    @Override
+    public MeterReadingDto update(Long meterReadingId, MeterReadingDto meterReadingDto) {
+
+        MeterReading meterReading = meterReadingRepo.findMeterReadingById(meterReadingId);
+
+        if(meterReadingDto.getMeterReading() < 0) {
+            meterReading.setMeterReading(meterReadingDto.getMeterReading());
+        }
+
+        if(meterReadingDto.getDate() != null) {
+            meterReading.setDate(meterReadingDto.getDate());
+        }
+
+        meterReadingRepo.save(meterReading);
+
+        return modelMapper.map(meterReading, MeterReadingDto.class);
+
+
+    }
+
+    @Override
+    public void delete(Long meterReadingId) {
+        meterReadingRepo.deleteMeterReadingById(meterReadingId);
+    }
 }
