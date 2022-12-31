57e8552d46082055f841a169a0388acccfac5b81
Kevin Muchene <muchenekevin@gmail.com>
2022-12-31 12:26:50 -0600

    Initial commit for this file

--- src/main/java/com/example/electricitybillingsystem/controller/CustomerController.java
+++ src/main/java/com/example/electricitybillingsystem/controller/CustomerController.java
@@ -1,4 +1,44 @@
 package com.example.electricitybillingsystem.controller;
 
+import com.example.electricitybillingsystem.entity.Customer;
+import com.example.electricitybillingsystem.entity.dto.response.CustomerDto;
+import com.example.electricitybillingsystem.service.implementation.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@CrossOrigin()
+
+@RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerServiceImpl customerService;
+
+    @GetMapping()
+    public List<CustomerDto> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public CustomerDto getCustomerById(@PathVariable("id") Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping()
+    public void saveCustomer(@RequestBody CustomerDto customerDto) {
+        customerService.save(customerDto);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCustomer(@PathVariable("id") Long customerId, @RequestBody CustomerDto customerDto) {
+        customerService.update(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable("id") Long id){
+        customerService.delete(id);
+    }
 }
